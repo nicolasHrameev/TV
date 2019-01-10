52d1560220327a24ce6b8723d1acc111d98d0522
nicolasHrameev <nicolas_hrameev@mail.ru>
2019-01-10 23:27:40 +0300

    Add files via upload

--- tv_return_service/tv_return_service.go
+++ tv_return_service/tv_return_service.go
@@ -22,50 +22,6 @@ type TV struct {
 
 const fileName = "tv_returns.xml"
 
-func ReturnTvs() error {
-	xmlFile, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer xmlFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(xmlFile)
-
-	var TvXmlInfo TvXml
-	err = xml.Unmarshal(byteValue, &TvXmlInfo)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	for index, TvXml := range TvXmlInfo.Tvs {
-		if TvXml.Readed != true {
-			TV, err := dbconn.GetTv(TvXml.ID)
-			if err != nil {
-				fmt.Println(err)
-				return err
-			}
-			count := TV.Count + TvXml.Count
-			if count < 0 {
-				fmt.Println("return data is incorrect")
-				return err
-			}
-			fmt.Printf("Executing return tvs. ID:%v, Count:%v, OldCount:%v\n", TV.ID, count, TV.Count)
-			err = dbconn.UpdateTvsCount(TV.ID, count)
-			if err != nil {
-				fmt.Println(err)
-				return err
-			}
-			fmt.Printf("Successful execute tvs. ID:%v, Count:%v, OldCount:%v\n", TV.ID, count, TV.Count)
-			TvXmlInfo.Tvs[index].Readed = true
-		}
-		modifiedXml, _ := xml.Marshal(TvXmlInfo)
-		err = ioutil.WriteFile(fileName, modifiedXml, 0644)
-	}
-	return nil
-}
-
 func ReadXML() (TvXml, error) {
 	xmlFile, err := os.Open(fileName)
 	if err != nil {
